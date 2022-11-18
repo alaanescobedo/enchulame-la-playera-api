c33ecb59757119a0e731302af38219fd632497b0
Alan Escobedo <alanescobedo.dev@gmail.com>
2022-11-18 01:56:25 -0600

    Revert "feat: repositorio y entidades modulo de usuario"

--- src/main/java/com/exercise/api/dtos/AuthUser.java
+++
@@ -1,9 +0,0 @@
-package com.exercise.api.dtos;
-
-import lombok.Data;
-
-@Data
-public class AuthUser {
-    private String username;
-    private String password;
-}
--- src/main/java/com/exercise/api/dtos/UserDto.java
+++
@@ -1,6 +0,0 @@
-package com.exercise.api.dtos;
-
-import com.exercise.api.entities.abstract_class.UserAbstract;
-
-public class UserDto extends UserAbstract {
-}
